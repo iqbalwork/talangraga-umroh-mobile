b51ed122eb00b1b27da65c8a6ec4432b63e66abb
Iqbal Fauzi <92508282+iqbalwork@users.noreply.github.com>
2025-11-05 07:04:50 +0700

    feat(auth): load bearer token (#3)

--- composeApp/src/commonMain/kotlin/com/talangraga/umrohmobile/data/local/session/SessionKey.kt
+++ composeApp/src/commonMain/kotlin/com/talangraga/umrohmobile/data/local/session/SessionKey.kt
@@ -2,6 +2,7 @@ package com.talangraga.umrohmobile.data.local.session
 
 object SessionKey {
     const val IS_LOGGED_IN = "is_logged_in"
-    const val TOKEN_KEY = "jwt_token"
+    const val TOKEN_KEY = "tokenKey"
+    const val REFRESH_TOKEN_KEY = "refreshtokenKey"
     const val PROFILE_KEY = "profile"
 }
